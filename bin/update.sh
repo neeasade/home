1a70a827e39cf2bd02ef5689b317d08f79fd8eac
Th3-Hum4n <visuwesh@gmail.com>
2018-05-25 17:52:52 +0530

    add vivaldi's custom ui

--- bin/update.sh
+++ bin/update.sh
@@ -1,1 +1,1 @@
-sudo pacman -Syu && sudo cp ~/tmp/common.css /opt/vivaldi-snapshot/resources/vivaldi/style/common.css && echo "copied" && read
+sudo pacman -Syu && sudo cp ~/bin/common.css /opt/vivaldi-snapshot/resources/vivaldi/style/common.css && echo "copied" && read
