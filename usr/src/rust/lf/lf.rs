f5e5f194a749a44644bc61193a9904ee8f7fb013
viz <viz@airmail.cc>
2019-04-17 13:43:28 +0530

    Add blacklist to lf

--- usr/src/rust/lf/lf.rs
+++ usr/src/rust/lf/lf.rs
@@ -1,5 +1,31 @@
 use std::{fs::{read_dir, canonicalize}, path::Path, env::args};
 
+fn is_in_blacklist(path: &str) -> bool {
+    let path = path.to_string();
+    let mut ret = false;
+    let blacklist: [&str; 8] = [
+                                "*.git*", "*cache*", "*Cache*", "*wallpapers*",
+                                "*lib*", "opt*", "*target*", "*tmp*"
+                               ];
+    for b in blacklist.iter() {
+        if ret { break; }
+
+        let b = b.to_string();
+        let i = b.replacen("*", "", 2);
+        let i = i.as_str();
+
+        if b.starts_with("*") && b.ends_with("*") {
+            ret = path.contains(i);
+        } else if b.starts_with("*") {
+            ret = path.starts_with(i);
+        } else if b.ends_with("*") {
+            ret = path.ends_with(i);
+        }
+    }
+
+    ret
+}
+
 fn walk(path: &str, fp: &str) {
     /* helper closurers */
     let is_dir = |path: &str| {
@@ -14,7 +40,11 @@ fn walk(path: &str, fp: &str) {
                  .into_os_string();
 
         let mut p = p.to_str()
-                 .unwrap();
+                     .unwrap();
+
+        if is_in_blacklist(&p) {
+            continue;
+        }
 
         if is_dir(&p) {
             walk(p, fp);
