cd07bfc12c9b38a768ed7cc198ff4486cd3fad39
viz <vizs@github.com>
2019-04-18 12:41:28 +0530

    etc update

--- etc/tm/dmenu.h
+++
@@ -1,7 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*                    fg      bg       */
-    [SchemeNorm]    =  { "#X7",  "#X0" },
-	[SchemeSel]     =  { "#X15", "#X8" },
-	[SchemeOut]     =  { "#000000", "#00ffff" },
-    [SchemeBorder]  =  { "#X8", "#X8" },
-};
