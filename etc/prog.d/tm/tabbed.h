91dc5d66841f71ec23f7683dfdb5fd68711098f9
viz <vizs@github.com>
2019-04-18 18:23:15 +0530

    oh

---
+++ etc/prog.d/tm/tabbed.h
@@ -0,0 +1,6 @@
+static const char* normbgcolor  = "#X8";
+static const char* normfgcolor  = "#X15";
+static const char* selbgcolor   = "#X0";
+static const char* selfgcolor   = "#X7";
+static const char* urgbgcolor   = "#X0";
+static const char* urgfgcolor   = "#X15";
