e70bb6159e5de8af6d116d912b635aed4537e4bc
viz <visuwesh@tutanota.com>
2020-12-31 15:04:14 +0530

    awesome: style

--- lib/awesome/rc.lua
+++ lib/awesome/rc.lua
@@ -21,29 +21,29 @@ local naughty = require("naughty")
 -- lol.
 
 if awesome.startup_errors then
-   naughty.naughty({
-		 preset = naughty.config.presets.critical,
-		 title = "Awesome config has an error",
-		 text = awesome.startup_errors,
-   })
+  naughty.naughty({
+      preset = naughty.config.presets.critical,
+      title = "Awesome config has an error",
+      text = awesome.startup_errors,
+  })
 end
 
 -- Also send notification on other errors
 
 do
-   local inerr = false
-   awesome.connect_signal(
-	  "debug::error",
-	  function (e)
-		 if inerr then return end
-		 inerr = true
-		 naughty.notify({
-			   preset = naughty.config.presets.critical,
-			   title = "An error in config",
-			   text = tostring(e),
-		 })
-		 inerr = false
-   end)
+  local inerr = false
+  awesome.connect_signal(
+    "debug::error",
+    function (e)
+      if inerr then return end
+      inerr = true
+      naughty.notify({
+          preset = naughty.config.presets.critical,
+          title = "An error in config",
+          text = tostring(e),
+      })
+      inerr = false
+  end)
 end
 
 -- * Wallpaper
@@ -56,37 +56,37 @@ local cairo = require("lgi").cairo
 -- This function is based on bgs' -z flag and
 -- https://stackoverflow.com/questions/6565703/math-algorithm-fit-image-to-screen-retain-aspect-ratio
 function vz_wallpaper_zoom (surf, s, background)
-   local geom, cr = gears.wallpaper.prepare_context(s)
-   local origsurf = surf
-   surf = gears.surface.load_uncached(surf)
-   background = gears.color(background)
+  local geom, cr = gears.wallpaper.prepare_context(s)
+  local origsurf = surf
+  surf = gears.surface.load_uncached(surf)
+  background = gears.color(background)
 
-   cr.operator = cairo.Operator.SOURCE
-   cr.source = background
-   cr:paint()
+  cr.operator = cairo.Operator.SOURCE
+  cr.source = background
+  cr:paint()
 
-   local w_i, h_i = gears.surface.get_size(surf)
-   local nw, nh = 0, 0
-   if (w_i / h_i) > (geom.width / geom.height) then
-	  nw = w_i * geom.height / h_i
-	  nh = geom.height
-   else
-	  nw = geom.width
-	  nh = h_i * geom.width / w_i
-   end
+  local w_i, h_i = gears.surface.get_size(surf)
+  local nw, nh = 0, 0
+  if (w_i / h_i) > (geom.width / geom.height) then
+    nw = w_i * geom.height / h_i
+    nh = geom.height
+  else
+    nw = geom.width
+    nh = h_i * geom.width / w_i
+  end
 
-   cr:translate((geom.width - nw)/2, (geom.height - nh)/2)
-   cr:rectangle(0, 0, nw, mw)
-   cr:scale(nw/w_i, nh/h_i)
+  cr:translate((geom.width - nw)/2, (geom.height - nh)/2)
+  cr:rectangle(0, 0, nw, mw)
+  cr:scale(nw/w_i, nh/h_i)
 
-   cr:set_source_surface(surf, 0, 0)
-   cr:paint()
-   if surf ~= origsurf then
-	  surf:finish()
-   end
-   if cr.status ~= "SUCCESS" then
-	  gears.debug.print_warning("Cairo context entered error state: " .. cr.status)
-   end
+  cr:set_source_surface(surf, 0, 0)
+  cr:paint()
+  if surf ~= origsurf then
+    surf:finish()
+  end
+  if cr.status ~= "SUCCESS" then
+    gears.debug.print_warning("Cairo context entered error state: " .. cr.status)
+  end
 end
 
 -- I have a script that will call `bgs` to set the wallpaper. And that
@@ -96,54 +96,54 @@ end
 -- accordingly!
 
 do
-   local path = gears.filesystem.get_xdg_cache_home() .. "wall"
-   local wallpaper = {}
-   if gears.filesystem.file_readable(path) then
-	  for i in io.lines(path) do
-		 if i then
-			for j in string.gmatch(i, "%S+") do
-			   table.insert(wallpaper, j)
-			end
-			break
-		 end
-	  end
-	  local mode = gears.wallpaper.fit
-	  local bg = "#000000"
+  local path = gears.filesystem.get_xdg_cache_home() .. "wall"
+  local wallpaper = {}
+  if gears.filesystem.file_readable(path) then
+    for i in io.lines(path) do
+      if i then
+        for j in string.gmatch(i, "%S+") do
+          table.insert(wallpaper, j)
+        end
+        break
+      end
+    end
+    local mode = gears.wallpaper.fit
+    local bg = "#000000"
 
-	  -- Set the path to wallpaper
-	  beautiful.wallpaper = wallpaper[2]
+    -- Set the path to wallpaper
+    beautiful.wallpaper = wallpaper[2]
 
-	  -- How to set the wallpaper can be determined from the rest of
-	  -- the flags
+    -- How to set the wallpaper can be determined from the rest of
+    -- the flags
 
-	  for i,f in pairs(wallpaper) do
-		 if f == "-C" then
-			bg = wallpaper[i+1]
-		 end
+    for i,f in pairs(wallpaper) do
+      if f == "-C" then
+        bg = wallpaper[i+1]
+      end
 
-		 -- c: centre
-		 -- z: zoom
-		 -- t: tile
-		 if f == "-c" then
-			mode = gears.wallpaper.centered
-			break
-		 elseif f == "-z" then
-			mode = vz_wallpaper_zoom
-			break
-		 elseif f == "-t" then
-			mode = gears.wallpaper.tiled
-			break
-		 end
-	  end
+      -- c: centre
+      -- z: zoom
+      -- t: tile
+      if f == "-c" then
+        mode = gears.wallpaper.centered
+        break
+      elseif f == "-z" then
+        mode = vz_wallpaper_zoom
+        break
+      elseif f == "-t" then
+        mode = gears.wallpaper.tiled
+        break
+      end
+    end
 
-	  awful.screen.connect_for_each_screen(function (s)
-			if mode ~= gears.wallpaper.tiled then
-			   mode(beautiful.wallpaper, s, bg)
-			else
-			   mode(beautiful.wallpaper, s)
-			end
-	  end)
-   end
+    awful.screen.connect_for_each_screen(function (s)
+        if mode ~= gears.wallpaper.tiled then
+          mode(beautiful.wallpaper, s, bg)
+        else
+          mode(beautiful.wallpaper, s)
+        end
+    end)
+  end
 end
 
 -- * Terminal and editor
@@ -155,21 +155,21 @@ editor = "emacsclient"
 editor_cmd = editor .. "-ca''"
 
 globalkeys = gears.table.join(
-   awful.key(
-	  { modkey, "Shift" }, "Return",
-	  function () awful.span(terminal) end,
-	  { description = "Spawn M-x shell or st if Emacs daemon is not running", group = "launcher" }),
-   awful.key(
-	  { modkey }, "e",
-	  function () awful.span(editor_cmd) end,
-	  { description = "Spawn emacsclient or start an Emacs session", group = "launcher" }))
+  awful.key(
+    { modkey, "Shift" }, "Return",
+    function () awful.span(terminal) end,
+    { description = "Spawn M-x shell or st if Emacs daemon is not running", group = "launcher" }),
+  awful.key(
+    { modkey }, "e",
+    function () awful.span(editor_cmd) end,
+    { description = "Spawn emacsclient or start an Emacs session", group = "launcher" }))
 
 -- * Layouts
 
 -- I don't find tiling useful™
 
 awful.layout.layouts = {
-   awful.layout.suit.floating,
+  awful.layout.suit.floating,
 }
 
 -- * Tags
@@ -179,38 +179,38 @@ awful.layout.layouts = {
 -- lua config. So yea....
 
 awful.screen.connect_for_each_screen(function (s)
-	  awful.tag({"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+    awful.tag({"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
 end)
 
 for i=1,10 do
-   globalkeys = gears.table.join(
-	  globalkeys,
-	  awful.key(
-		 { modkey },
-		 "#"..i+9,
-		 function ()
-			local tag = awful.screen.focused().tags[i]
-			if tag then tag:view_only() end
-		 end,
-		 { description = "Switch to tag #"..i, group = "tag" }),
-	  awful.key(
-		 { modkey, "Control" },
-		 "#"..i+9,
-		 function ()
-			local tag = awful.screen.focused().tags[i]
-			if tag then awful.tag.viewtoggle(tag) end
-		 end,
-		 { description = "Toggle tag #"..i, group = "tag" }),
-	  awful.key(
-		 { modkey, "Shift" },
-		 "#"..i+9,
-		 function ()
-			if client.focus then
-			   local tag = client.focus.screen.tags[i]
-			   if tag then client.focus:move_to_tag(tag) end
-			end
-		 end,
-		 { description = "Move focused window to tag #"..i, group = "tag" }))
+  globalkeys = gears.table.join(
+    globalkeys,
+    awful.key(
+      { modkey },
+      "#"..i+9,
+      function ()
+        local tag = awful.screen.focused().tags[i]
+        if tag then tag:view_only() end
+      end,
+      { description = "Switch to tag #"..i, group = "tag" }),
+    awful.key(
+      { modkey, "Control" },
+      "#"..i+9,
+      function ()
+        local tag = awful.screen.focused().tags[i]
+        if tag then awful.tag.viewtoggle(tag) end
+      end,
+      { description = "Toggle tag #"..i, group = "tag" }),
+    awful.key(
+      { modkey, "Shift" },
+      "#"..i+9,
+      function ()
+        if client.focus then
+          local tag = client.focus.screen.tags[i]
+          if tag then client.focus:move_to_tag(tag) end
+        end
+      end,
+      { description = "Move focused window to tag #"..i, group = "tag" }))
 end
 
 -- * Keybinds
@@ -220,23 +220,23 @@ end
 -- joke though, they are actually good
 
 for d,c in pairs({ w = { 0, -50 }, s = { 50, 0 },
-				   a = { -50, 0 }, d = { 0, -50 } }) do
-   globalkeys = gears.table.join(
-	  globalkeys,
-	  awful.key(
-		 { modkey }, d,
-		 function ()
-			if client.focus then
-			   client.focus.moveresize(c[1], c[2])
-			end
-	  end),
-	  awful.key(
-		 { modkey, "Shift" }, d,
-		 function ()
-			if client.focus then
-			   client.focus.moveresize(0, 0, c[1], c[2])
-			end
-   end))
+                   a = { -50, 0 }, d = { 0, -50 } }) do
+  globalkeys = gears.table.join(
+    globalkeys,
+    awful.key(
+      { modkey }, d,
+      function ()
+        if client.focus then
+          client.focus.moveresize(c[1], c[2])
+        end
+    end),
+    awful.key(
+      { modkey, "Shift" }, d,
+      function ()
+        if client.focus then
+          client.focus.moveresize(0, 0, c[1], c[2])
+        end
+  end))
 end
 
 -- ** Window focus
@@ -244,10 +244,10 @@ end
 -- Directional focus is the only sane option!
 
 for k,d in pairs({ h = "left", j = "down",
-				   k = "up", l = "right" }) do
-   globalkeys = gears.table.join(
-	  globalkeys,
-	  awful.key({ modkey, "Control" }, k, function () awful.client.focus.bydirection(d) end))
+                   k = "up", l = "right" }) do
+  globalkeys = gears.table.join(
+    globalkeys,
+    awful.key({ modkey, "Control" }, k, function () awful.client.focus.bydirection(d) end))
 end
 
 -- Sometimes I lose focus and flashing the window border can
@@ -256,91 +256,91 @@ end
 -- local vz_flashfocus = false
 
 -- globalkeys = gears.table.join(
---    globalkeys,
---    awful.key({ modkey, "Shift" }, "f",
--- 	  function ()
--- 		 if vz_flashfocus then
--- 			vz_flashfocus = false
--- 		 end
--- 	  end,
--- 	  { description = "Flash the border of focused client" }))
+--   globalkeys,
+--   awful.key({ modkey, "Shift" }, "f",
+--     function ()
+--       if vz_flashfocus then
+--         vz_flashfocus = false
+--       end
+--     end,
+--     { description = "Flash the border of focused client" }))
 
 -- ** Awesome independent commands
 
 -- These are with just modkey modifier
 for k,c in pairs({
-	  Print = "screenshot -u",
-	  v = "chromium",
-	  l = "slock",
-	  m = "mus",
-	  -- p = "wmenu",
+    Print = "screenshot -u",
+    v = "chromium",
+    l = "slock",
+    m = "mus",
+    -- p = "wmenu",
 }) do
-   globalkeys = gears.table.join(
-	  globalkeys,
-	  awful.key({ modkey }, k, function () awful.spawn(c) end))
+  globalkeys = gears.table.join(
+    globalkeys,
+    awful.key({ modkey }, k, function () awful.spawn(c) end))
 end
 
 -- These are with shift modifier
 for k,c in pairs({
-	  x = "turnoff",
-	  r = "pkill -USR1 redshift",
-	  -- f = "flashfocus",
-	  k = "vol -i 1%",
-	  j = "vol -d 1%",
-	  m = "vol -t",
-	  l = "doas $HOME/bin/brness -i 1",
-	  h = "doas $HOME/bin/brness -d 1",
+    x = "turnoff",
+    r = "pkill -USR1 redshift",
+    -- f = "flashfocus",
+    k = "vol -i 1%",
+    j = "vol -d 1%",
+    m = "vol -t",
+    l = "doas $HOME/bin/brness -i 1",
+    h = "doas $HOME/bin/brness -d 1",
 }) do
-   globalkeys = gears.table.join(
-	  globalkeys,
-	  awful.key({ modkey, "Shift" }, k, function () awful.spawn(c) end))
+  globalkeys = gears.table.join(
+    globalkeys,
+    awful.key({ modkey, "Shift" }, k, function () awful.spawn(c) end))
 end
 
 -- ** Notifications
 
 for k,n in pairs({
-	  b = { "battery", "bat -p" },
-	  t = { "time", "date +%H:%M" },
-	  v = { "volume", "vol -g" },
+    b = { "battery", "bat -p" },
+    t = { "time", "date +%H:%M" },
+    v = { "volume", "vol -g" },
 }) do
-   globalkeys = gears.table.join(
-	  globalkeys,
-	  awful.key({ modkey, "Shift" }, k,
-		 function ()
-			awful.spawn.easy_async(
-			   n[2],
-			   function (out) naughty.notification({ title = n[1], message = out }) end)
-   end))
+  globalkeys = gears.table.join(
+    globalkeys,
+    awful.key({ modkey, "Shift" }, k,
+      function ()
+        awful.spawn.easy_async(
+          n[2],
+          function (out) naughty.notification({ title = n[1], message = out }) end)
+  end))
 end
 
 -- ** Reloading and quitting awesome
 
 globalkeys = gears.table.join(
-   globalkeys,
-   awful.key({ modkey, "Shift" }, "q", awesome.restart),
-   awful.key({ modkey, "Shift", "Control" }, "q", awesome.quit))
+  globalkeys,
+  awful.key({ modkey, "Shift" }, "q", awesome.restart),
+  awful.key({ modkey, "Shift", "Control" }, "q", awesome.quit))
 
 -- * Rules
 
 -- Woooo Rules are nice to have around LOL
 
 awful.rules.rule = {
-   {
-	  rule_any = { role = "browser" },
-	  properties = { tag = "2" },
-   },
-   {
-	  rule = { class = "mpv", instance = "mpv-popup" },
-	  properties = {
-		 floating = true,
-		 -- placement = awful.placement.top_left,
-		 titlebar_enabled = false,
-		 is_fixed = true,
-		 sticky = true,
-		 ontop = true,
-		 border_width = 0,
-	  },
-   },
+  {
+    rule_any = { role = "browser" },
+    properties = { tag = "2" },
+  },
+  {
+    rule = { class = "mpv", instance = "mpv-popup" },
+    properties = {
+      floating = true,
+      -- placement = awful.placement.top_left,
+      titlebar_enabled = false,
+      is_fixed = true,
+      sticky = true,
+      ontop = true,
+      border_width = 0,
+    },
+  },
 }
 
 -- * Mousebinds
@@ -348,35 +348,39 @@ awful.rules.rule = {
 -- Mouse is useful, yes.
 
 vz_rmenu = awful.menu(
-   { items = {
-		{
-		   "new",
-		   function ()
-			  awful.spawn.easy_async(
-				 "crud",
-				 function (out)
-					local geo, j = {}, 1
-					for i in string.gmatch(out, "%S+") do
-					   geo[j] = tonumber(i)
-					   j = j+1
-					end
-					awful.spawn(terminal, { x = geo[1], y = geo[2], w = geo[3], h = geo[4] })
-			  end)
-		   end,
-		},
-		{
-		   "reshape",
-		   function ()
+  { items = {
+      {
+        "new",
+        function ()
+          awful.spawn.easy_async(
+            "crud",
+            function (out)
+              local geo, j = {}, 1
+              for i in string.gmatch(out, "%S+") do
+                geo[j] = tonumber(i)
+                j = j+1
+              end
+              awful.spawn(terminal, { x = geo[1], y = geo[2], w = geo[3], h = geo[4] })
+          end)
+        end,
+      },
+      {
+        "reshape",
+        function ()
 
-		   end
-		},
+        end
+      },
 }})
 
 
 root.buttons(
-   awful.key(
-	  {}, 1, function () vz_rmenu:toggle() end))
+  awful.key(
+    {}, 1, function () vz_rmenu:toggle() end))
 
 -- * -
--- Local Variables: eval: (outline-minor-mode)
--- outline-regexp: "-- [*]+" End:
+-- Local Variables:
+-- eval: (outline-minor-mode)
+-- lua-indent-level: 2
+-- indent-tabs-mode: nil
+-- outline-regexp: "-- [*]+"
+-- End:
